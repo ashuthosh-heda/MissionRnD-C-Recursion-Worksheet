ad2bb4ff187bf4c3478f009061f7f5b4aae193cd
ashuthosh-heda <ashuthosh-heda@missionrnd.com>
2016-03-28 19:02:26 +0530

    Submit Commit

--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -34,7 +34,47 @@ Note : Check the function Parameters ,Its a double pointer .
 #include "stdafx.h"
 #include<stdlib.h>
 
+void spiralUtil(int **input, int rows, int columns, int *res,int ind,int inner){
+	if (rows <= 0 || columns <= 0)
+		return;
+
+	if (rows == 1){
+		for (int i = 0; i < columns; i++)
+			res[ind++] = input[inner][inner + i];
+		return;
+	}
+
+	if (columns == 1){
+		for (int i = 0; i < rows; i++)
+			res[ind++] = input[inner + i][inner];
+		return;
+	}
+
+	for (int i = 0; i < columns - 1; i++)
+		res[ind++] = input[inner][inner + i];
+
+	for (int i = 0; i < rows - 1; i++)
+		res[ind++] = input[inner + i][inner + columns - 1];
+
+	for (int i = 0; i < columns - 1; i++)
+		res[ind++] = input[inner + rows - 1][inner + columns - 1 - i];
+
+	for (int i = 0; i < rows - 1; i++)
+		res[ind++] = input[inner + rows - 1 - i][inner];
+
+	spiralUtil(input, rows - 2, columns - 2, res, ind, inner + 1);
+}
+
 int *spiral(int rows, int columns, int **input_array)
 {
-	return NULL;
+	if (rows <= 0 || columns <= 0)
+		return NULL;
+	if (input_array == NULL)
+		return NULL;
+	
+	int *res = (int*)calloc(rows*columns, sizeof(int));
+
+	spiralUtil(input_array, rows, columns, res, 0, 0);
+
+	return res;
 }
